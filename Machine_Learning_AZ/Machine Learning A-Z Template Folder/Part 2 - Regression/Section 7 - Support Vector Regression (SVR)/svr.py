75945287ec32b238ddaf62b5f9fc68915094d0c8
Zhen Song <zhen.song@siemens.com>
2018-08-20 06:53:35 -0500

    run SVR"

--- Machine_Learning_AZ/Machine Learning A-Z Template Folder/Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/svr.py
+++ Machine_Learning_AZ/Machine Learning A-Z Template Folder/Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/svr.py
@@ -10,6 +10,7 @@ dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
+
 # Splitting the dataset into the Training set and Test set
 """from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
@@ -19,7 +20,7 @@ from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 sc_y = StandardScaler()
 X = sc_X.fit_transform(X)
-y = sc_y.fit_transform(y)
+y = sc_y.fit_transform(y.reshape(-1,1))
 
 # Fitting SVR to the dataset
 from sklearn.svm import SVR
